95715d076d2e7d8885472e455a7f3a9180a0280a
Ryoo-HS <binisoo12@gmail.com>
2026-03-06 20:07:23 +0900

    MATLAB프로그래밍 1주차 과제
    백준 단계별 풀이 1단계 전체

---
+++ 백준 1주차.py
@@ -0,0 +1,70 @@
+#2557
+print("Hello World!")
+
+#1000
+A, B = map(int, input().split())
+print(A+B)
+
+#1001
+A, B = map(int, input().split())
+print(A-B)
+
+#10998
+A, B = map(int, input().split())
+print(A*B)
+
+#1008
+A, B = map(int, input().split())
+print(A/B)
+
+#10869
+A, B = map(int, input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(A//B)
+print(A%B)
+
+#10926
+a= input()
+print(a+"??!")
+
+#18108
+a = int(input())
+print(a-543)
+
+#10430
+a, b, c = map(int, input().split())
+
+print((a+b)%c)
+print((a%c+b%c)%c)
+print((a*b)%c)
+print((a%c*b%c)%c)
+
+#2588
+a = int(input())
+b = int(input())
+
+q = b%10
+w = (b%100)//10
+e = b//100
+
+print(a*q)
+print(a*w)
+print(a*e)
+print(a*b)
+
+
+#11832
+
+a,b,c = map(int, input().split())
+
+print(a+b+c)
+
+#10171역슬래쉬 이상
+
+print("＼    /＼\n )  ( ')\n(  /  )\n ＼(__)|")
+
+#10172역슬래쉬 이상
+
+print('''|＼_/|\n|q p|   /}\n( 0 )"""＼\n|"^"`    |\n||_/=＼＼__|''')
